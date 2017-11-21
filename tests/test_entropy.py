e163b178b97ff6d015d3fe24387ec45bbc7042b1
Pau Rué <pau.rue@schibsted.com>
2017-11-21 16:40:35 +0100

    Changed unittest to pytest

--- tests/test_entropy.py
+++ tests/test_entropy.py
@@ -1,42 +1,42 @@
-import unittest
+import pytest
 import numpy as np
 from claude.measures import information_entropy
 
 
-class TestEntropy(unittest.TestCase):
-    SAMPLE_SIZE = 10000
-    SAMPLE_SIZE_SMALL = 100
+SAMPLE_SIZE = 10000
+SAMPLE_SIZE_SMALL = 100
+
+
+def within_tolerance(value1, value2):
     TOLERANCE = 1e-3
-
-    def within_tolerance(self, value1, value2):
-        return np.abs(value1 - value2) < self.TOLERANCE
-
-    def test_zero_entropy(self):
-        x = np.ones(self.SAMPLE_SIZE_SMALL)
-        self.assertEqual(information_entropy(x), 0)
-
-    def test_max_entropy_1(self):
-        x0 = np.zeros(self.SAMPLE_SIZE_SMALL)
-        x1 = np.ones(self.SAMPLE_SIZE_SMALL)
-        x = np.concatenate((x0, x1))
-        self.assertEqual(information_entropy(x), 1)
-
-    def test_max_entropy_2(self):
-        x = np.concatenate([i * np.ones(self.SAMPLE_SIZE_SMALL)
-                            for i in range(4)])
-        self.assertEqual(information_entropy(x), 2)
-
-    def test_entropy_1(self):
-        x = np.random.random(self.SAMPLE_SIZE) > 0.5
-        self.assertEqual(self.within_tolerance(information_entropy(x), 1),
-                         True)
-
-    def test_entropy_2(self):
-        sample_size = 10
-        x = np.arange(sample_size)
-        self.assertEqual(self.within_tolerance(information_entropy(x),
-                                               np.log2(sample_size)), True)
+    return np.abs(value1 - value2) < TOLERANCE
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_zero_entropy():
+    x = np.ones(SAMPLE_SIZE_SMALL)
+    assert information_entropy(x) == 0
+
+
+def test_max_entropy_1():
+    x0 = np.zeros(SAMPLE_SIZE_SMALL)
+    x1 = np.ones(SAMPLE_SIZE_SMALL)
+    x = np.concatenate((x0, x1))
+    assert information_entropy(x) == 1
+
+
+def test_max_entropy_2():
+    x = np.concatenate([i * np.ones(SAMPLE_SIZE_SMALL)
+                        for i in range(4)])
+    assert information_entropy(x) == 2
+
+
+def test_entropy_1():
+    x = np.random.random(SAMPLE_SIZE) > 0.5
+    assert within_tolerance(information_entropy(x), 1)
+
+
+def test_entropy_2():
+    sample_size = 10
+    x = np.arange(sample_size)
+    assert within_tolerance(information_entropy(x),
+                            np.log2(sample_size))
